e936d85eb8c24344d182318890231ba6c38abae5
prakash <prakashtech250@gmail.com>
2024-03-30 15:00:01 +0545

    change

--- api/app.py
+++ api/app.py
@@ -236,19 +236,12 @@ app = Flask(__name__)
 def index():
     return 'Amazon reviews'
 
-@app.route('/reviews', methods=['POST'])
-def reviews():
+@app.route('/reviews/<country>/<asin>', methods=['GET'])
+def reviews(country, asin):
     data = request.form
     page = 1
-    if 'asin' in data and 'country' in data:
-        asin = data['asin']
-        country = data['country']
-        if 'page' in data:
-            # page = data['page']
-            pass
-        return jsonify(amApi.get_reviews(asin, country, page))
-    else:
-        return jsonify({'Error': 'Asin and Country not provided'})
+    country = country.upper()
+    return jsonify(amApi.get_reviews(asin, country, page))
 
 
 if __name__== "__main__":
